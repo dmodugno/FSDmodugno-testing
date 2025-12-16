8b39304d4b83b620ec251d57bbeab497a468f1ed
dmodugno <dmodugno@churchofjesuschrist.org>
2025-12-16 18:33:13 -0500

    Configure GitHub Pages deployment

--- origin-cards-ab-test/vite.config.js
+++ origin-cards-ab-test/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/FSDmodugno-testing/',
   publicDir: 'assets',
   build: {
     outDir: 'dist'
